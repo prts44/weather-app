5436143a05f6f81f0f37fe05b59096fed8869dbc
z <ryan.ckit57@gmail.com>
2022-08-02 19:48:16 -0400

    Basic API call for weather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+import { apiKey } from './key.js';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState(null);
+    const [display, setDisplay] = useState(<></>);
+
+    function getWeatherData() {
+        fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + 'Canada' + '/today?unitGroup=us&key=' + apiKey + '&contentType=json')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data); 
+                setData(data);
+                setDisplay(<>
+                    <p>{data.latitude}</p>
+                    <p>{data.longitude}</p>
+                    <p>{data.address}</p>
+                </>);
+            });
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <button onClick={() => {getWeatherData();} }>Get data</button>
+                {display}
+            </header>
+        </div>
+    );
 }
 
 export default App;
